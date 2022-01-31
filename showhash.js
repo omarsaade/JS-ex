b8c4f47c62235d54e0f879198a180520f087e2b2
omar saade <omarsaadeh11@gmail.com>
2022-01-31 23:35:47 +0200

    Check if Url Contains String

---
+++ showhash.js
@@ -0,0 +1,34 @@
+ex1
+if (window.location.hash) {
+    if (window.location.hash.indexOf("osama") === 1) {
+
+    }
+} else {
+    console.log("oh no");
+}
+
+ex2
+if (window.location.hash) {
+    var hash = window.location.hash.substring(1);
+
+} else {
+    console.log("oh no");
+}
+
+/////////////////
+
+ex3
+
+
+
+
+
+if (window.location.hash) {
+    var hash = window.location.hash.substring(1);
+    if (hash = "omar") {
+        console.log("good job");
+    }
+
+} else {
+    console.log("oh no");
+}
