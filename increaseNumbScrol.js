8461ddea58b59cd941590e48eb3d7383f5435e8a
omar saade <omarsaadeh11@gmail.com>
2022-03-17 23:57:59 +0200

    increase numbers on scrolling

--- increaseNumbScrol.js
+++ increaseNumbScrol.js
@@ -1,3 +1,32 @@
+let nums = document.querySelectorAll('.nums .num');
+let section = document.querySelector(".three");
+//trigger one time only
+let started = false; //function started ? no
+
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop) {
+        if (!started) { //if true fut 3laya
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;//BTERJA3 BT3ABI FALSE MAHAL EL
+        //  TRUE W MA BI3UD 2ADER YFUT 3AL CONDITION
+    }
+};
+
+
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
+}
+
+
 
 
 
